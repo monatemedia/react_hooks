96a1162bbb928516048cc7206f4d47780182ed41
Edward Baitsewe <edward@monatemedia.com>
2023-03-13 22:56:18 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,114 @@
+// import React and the useState hook
+import { useState } from "react";
+import "./styles.css";
+
+// component function
+function SimpleArrayOfObjectsComponent() {
+  // set the initial state (an array with 1 object to start (this can be an empty object to start))
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      name: "Joe",
+      type: "admin"
+    }
+  ]);
+  // boolean state to know if we are editing (this will let us display
+  const [isEditing, setIsEditing] = useState(false);
+  // object state to set so we know which todo item we are editing
+  const [currentUser, setCurrentUser] = useState({});
+
+  // delcare the function
+  function handleAddNewUser() {
+    // it's important to not mutate state directly, so here we are creating a copy of the current state using the spread syntax
+    const updateUsers = [
+      // copy the current users state
+      ...users,
+      // now you can add a new object to add to the array
+      {
+        // using the length of the array for a unique id
+        id: users.length + 1,
+        // adding a new user name
+        name: "Steve",
+        // with a type of member
+        type: "member"
+      }
+    ];
+    // update the state to the updatedUsers
+    setUsers(updateUsers);
+  }
+
+  // function to get the value of the edit input and set the new state
+  function handleEditInputChange(e) {
+    // set the new state value to what's currently in the edit input box
+    setCurrentUser({ ...currentUser, name: e.target.value });
+  }
+
+  // function to handle when the "Edit user name" button is clicked
+  function handleEditClick(user) {
+    // set isEditing to true
+    setIsEditing(true);
+    // update the state to the updatedUsers
+    setCurrentUser({ ...user });
+  }
+
+  function handleUpdateUser(id, updatedUser) {
+    // here we are mapping over the users array - the idea is check if the user.id matches the id we pass into the function
+    // if the id's match, use the second parameter to pass in the updated user object
+    // otherwise just use old user
+    const updatedObject = users.map((user) =>
+      user.id === id ? updatedUser : user
+    );
+    // set editing to false because this function will be used inside an onSubmit function - which means the data was submited and we are no longer editing
+    setIsEditing(false);
+    // update the users state with the updated user
+    setUsers(updatedObject);
+  }
+
+  function handleEditFormSubmit(e) {
+    e.preventDefault();
+
+    // call the handleUpdateTodo function - passing the currentUser.id and the currentUser object as arguments
+    handleUpdateUser(currentUser.id, currentUser);
+  }
+
+  // JSX we want to return
+  return (
+    // parent div to hold the ul and li's
+    <div className="App">
+      {/* start - if the "edit user name" is clicked */}
+      {currentUser.id && isEditing && (
+        <form onSubmit={handleEditFormSubmit}>
+          <input
+            name="editTodo"
+            type="text"
+            placeholder="Edit todo"
+            value={currentUser.name}
+            onChange={handleEditInputChange}
+          />
+          <button type="submit">Update</button>
+          <button onClick={() => setIsEditing(false)}>Cancel</button>
+        </form>
+      )}
+      {/* end - edit form */}
+      <button onClick={handleAddNewUser}>Add new user</button>
+      <ul>
+        {/* map over the users array */}
+        {users.map((user) => (
+          // display a <div> element with the user.name and user.type
+          // parent element needs to have a unique key
+          <div key={user.id}>
+            <p>{user.name}</p>
+            <p>{user.type}</p>
+            {!isEditing && (
+              <button onClick={() => handleEditClick(user)}>
+                Edit user name
+              </button>
+            )}
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SimpleArrayOfObjectsComponent;
